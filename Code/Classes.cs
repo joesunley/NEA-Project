c14032b1340960d95260f8b3b90f01273543eb53
joesunley <joe@jossy.co.uk>
2020-07-22 18:05:05 +0100

    Update to Classes.cs
    Added Constructor Functions for
    - Mp Race
    - Comp Race
    - Player
    -Host
    
    Commented these Constructor Functions with a summary of the Function and info on each of the parameters
    
    Added some variables to the SinglePlayer Race but haven't added any constructors
    
    Need to finish the 2nd constructor for Host because haven't added Properties to the classes, which will be done in next commit
    
    Removed a couple of variables that weren't used
    
    Need to check how interfaces work as can't get to protected variables

--- Code/Classes.cs
+++ Code/Classes.cs
@@ -45,6 +45,10 @@ public interface IRace
 
 public class SpRace : IRace
 {
+
+    protected ResultsFile resultsFile;
+    protected Map map;
+
     public SpRace()
     {
 
@@ -59,10 +63,6 @@ public class MpRace : IRace
     protected Host host;
     protected string ipAddress;
 
-
-
-    protected Host host;
-    protected string ipAddress;
     protected string raceName;
     protected string startInterval;
     protected string weather;
@@ -71,10 +71,32 @@ public class MpRace : IRace
     protected ResultsFile resultsFile;
     protected Map map;
 
-
+    /// <summary>
+    /// Blank Constuctor Function for a MultiPlayer Race
+    /// </summary>
     public MpRace()
     {
 
+    }
+    /// <summary>
+    /// Contstructor Function for a MultiPlayer Race
+    /// </summary>
+    /// <param name="h">The Host for this race</param>
+    /// <param name="ip">The Host's ip Address</param>
+    /// <param name="rN">The name of the race</param>
+    /// <param name="sI">The start interval: Mass Start, 15, 3[, 45, 60, 120</param>
+    /// <param name="wT">The weather during the race: Sunny, Raining, Sunny</param>
+    /// <param name="night">Whether the race is at night or not</param>
+    /// <param name="m">The Map for the race</param>
+    public MpRace(Host h, string ip, string rN, string sI, string wT, bool night, Map m)
+    {
+        this.host = h;
+        this.ipAddress = ip;
+        this.raceName = rN;
+        this.startInterval = sI;
+        this.weather = wT;
+        this.NightMode = night;
+        this.map = m;
     }
 }
 
@@ -83,24 +105,51 @@ public class CpRace : IRace
 
     protected string CompID;
 
+    /// <summary>
+    /// Blank Constructor Function for a Competition Race
+    /// </summary>
     public CpRace()
     {
 
     }
+
+    /// <summary>
+    /// Constructor Function for a Competition Race
+    /// </summary>
+    /// <param name="cID">The Catching Features Competition ID for this Race (Can be found on the CF Website</param>
+    public CpRace(string cID) { this.CompID = cID; }
 }
 
 public class Player
 {
-    protected int iD;
+    
     protected string name;
     protected string email;
     protected string club;
-    protected string cfUsername;
     protected string username;
+    
 
+    /// <summary>
+    /// Blank Constructor Function for a Player
+    /// </summary>
     public Player()
     {
 
+    }
+
+    /// <summary>
+    /// Constructor Function for a Player
+    /// </summary>
+    /// <param name="n">The Players name</param>
+    /// <param name="e">The Players Email Address</param>
+    /// <param name="cl">The Players Club</param>
+    /// <param name="u">The Players Catching Features Username</param>
+    public Player(string n, string e, string cl, string u)
+    {
+        this.name = n;
+        this.email = e;
+        this.club = cl;
+        this.username = u;
     }
 }
 
@@ -108,8 +157,41 @@ public class Host : Player
 {
     protected string iPAddress;
 
-    public Host() : base(Player)
+    /// <summary>
+    /// Blank Constructor Function for a Host
+    /// </summary>
+    public Host()
     {
+        
+    }
+
+    /// <summary>
+    /// Constructor Function for a Host
+    /// </summary>
+    /// <param name="n">The Players name</param>
+    /// <param name="e">The Players Email Address</param>
+    /// <param name="cl">The Players Club</param>
+    /// <param name="u">The Players Catching Features Username</param>
+    /// <param name="ip">The Host's IP Address</param>
+    public Host(string n, string e, string cl, string U, string ip)
+    {
+        this.name = n;
+        this.email = e;
+        this.club = cl;
+        this.username = U;
+        this.iPAddress = ip;
+    }
+
+    /// <summary>
+    /// Constructor Function for a Host that uses a already existing Person
+    /// </summary>
+    /// <param name="player">A Player that has previously been created</param>
+    /// <param name="ip">The Host's IP Address</param>
+    public Host(Player player, string ip)
+    {
+        // ADD Player Details (properties)
+
+        this.iPAddress = ip;
 
     }
 }
