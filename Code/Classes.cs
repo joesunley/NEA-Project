fb2a11b572cf251f5a52185795c6b69c62acdeb5
Joe Sunley <57263040+joesunley@users.noreply.github.com>
2020-09-11 09:31:48 +0100

    Updated Classes.cs
    Added the CheckEmail Function
    
    The full write up of the function is at: /Functions/CheckEmail.md
    
    #19

--- Code/Classes.cs
+++ Code/Classes.cs
@@ -62,7 +62,7 @@ public class Round
         for (int i = 0; i < groupCount; i += 1) { this.groups.Add(new Group()); }
 
         this.races = race;
-            
+
         this.startingCompetitors = competitors;
     }
 
@@ -410,7 +410,43 @@ public class Player
         }
     }
 
-    private bool CheckEmail(string email) { }
+    private bool CheckEmail(string email)
+    {
+        const string acceptableDomainChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.";
+        const string acceptableStartChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        const string specialChars = "!#$%&'*+-/=?^_`.{|}~";
+
+        string[] splitForAt; // Splits the text at the @ symbo
+
+        try
+        {
+            splitForAt = email.Split('@');
+
+            if (splitForAt.Length != 2) { return false; }
+        }
+        catch { return false; } // Does not contain '@' symbol
+
+        string domain = splitForAt[1];
+
+        if (domain.Contains('.')) { } else { return false; }
+
+        for (int i = 0; i < domain.Length; i += 1)
+        {
+            if (acceptableDomainChars.Contains(domain[i])) { } else { return false; }
+        }
+
+        if (acceptableStartChars.Contains(email[0])) { } else { return false; }
+
+        for (int i = 0; i < email.Length - 1; i += 1)
+        {
+            if (specialChars.Contains(email[i]))
+            {
+                if (email[i + 1] == email[i]) { return false; }
+            }
+        }
+
+        return true;
+    } // https://github.com/joesunley/NEA-Project/blob/master/Functions/CheckEmail.md
 
     /// <summary>
     /// Public property for Player.club
