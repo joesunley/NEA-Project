01fee6b100d3175571a64740491240e86d73be80
Joe Sunley <57263040+joesunley@users.noreply.github.com>
2020-09-15 09:41:14 +0100

    Update Map.cs
    Added the imports
    Removed some whitespace

--- Code/Competition Classses/Map.cs
+++ Code/Competition Classses/Map.cs
@@ -1,3 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
 public class Map
 {
 
@@ -7,13 +18,10 @@ public class Map
     string mapMapper = "";
     string mapDescription = "";
 
-
     public Map()
     {
 
     }
-
-
 
     public bool CheckMap(Map map) { }
 }
