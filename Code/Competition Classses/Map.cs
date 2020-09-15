d8a7fa3246241f9b91bf272389853fe170379b8d
Joe Sunley <57263040+joesunley@users.noreply.github.com>
2020-09-15 09:31:33 +0100

    Create Map.cs

---
+++ Code/Competition Classses/Map.cs
@@ -0,0 +1,19 @@
+public class Map
+{
+
+    string hostFileLocation = "";
+
+    string mapName = "";
+    string mapMapper = "";
+    string mapDescription = "";
+
+
+    public Map()
+    {
+
+    }
+
+
+
+    public bool CheckMap(Map map) { }
+}
