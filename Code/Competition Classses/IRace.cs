6be3eb9aa74d10612768336edbcba02d63db1215
Joe Sunley <57263040+joesunley@users.noreply.github.com>
2020-09-15 09:40:31 +0100

    Update IRace.cs
    Added the imports

--- Code/Competition Classses/IRace.cs
+++ Code/Competition Classses/IRace.cs
@@ -1,3 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
 public interface IRace
 {
 
